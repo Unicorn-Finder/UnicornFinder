b55bdadc670d41ad709ba32a2f718a975844a716
yoo2wo <wogud758787@naver.com>
2022-03-11 12:16:14 +0900

    setting

---
+++ src/main/java/unicornfinder/unicornfinder/UnicornFinderApplication.java
@@ -0,0 +1,13 @@
+package unicornfinder.unicornfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicornFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicornFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unicornfinder/unicornfinder/UnicornFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package unicornfinder.unicornfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicornFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
