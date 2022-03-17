8db6d5ceca90596e30f9bc7f71e5e0ada6610d3d
Goun96 <gowoun96@gmail.com>
2022-03-17 22:11:17 +0900

    FEAT : CompanyServiceTest Test 생성 후 실행 Repository에서 query like 문 오류수정

--- src/main/java/unicornfinder/unicornfinder/repository/CompanyRepository.java
+++ src/main/java/unicornfinder/unicornfinder/repository/CompanyRepository.java
@@ -27,19 +27,20 @@ public class CompanyRepository {
 
     //company count 값 설정
     private int setCount(Long id) {
-        List<Interest> interests = em.createQuery("select i from Interest i" +
-                "where i.company_id = :company_id", Interest.class)
-                .setParameter("company_id", id)
+        List<Interest> interests = em.createQuery("select i from Interest i " +
+                "where i.companyId.id = :companyid", Interest.class)
+                .setParameter("companyid", id)
                 .getResultList();
         return interests.size();
     }
 
     //회사이름이나 서비스명 검색하기
+    // !프런트에서 작업해도 될거같지만 일단 리포지토리를 만들어봤음
     public List<Company> findByName_Product(String search){
 
-        List<Company> companyList = em.createQuery("select c from Company c" +
-                "where c.name like %:search% or c.product like %:search%", Company.class)
-                .setParameter("search", search)
+        List<Company> companyList = em.createQuery("select c from Company c " +
+                "where c.name like :search or c.product like :search", Company.class)
+                .setParameter("search", "%"+search+"%")
                 .getResultList();
 
         //조회한 company에 count값 설정해주기
@@ -48,5 +49,15 @@ public class CompanyRepository {
         }
 
         return companyList;
+    }
+
+    //company 등록(임시테스트용)
+    public void save(Company company){
+        em.persist(company);
+    }
+
+    //interest 등로 임시테스트용)
+    public void saveInterest(Interest interest){
+        em.persist(interest);
     }
 }
--- src/test/java/unicornfinder/unicornfinder/service/CompanyServiceTest.java
+++ src/test/java/unicornfinder/unicornfinder/service/CompanyServiceTest.java
@@ -7,8 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import unicornfinder.unicornfinder.domain.Company;
+import unicornfinder.unicornfinder.domain.Interest;
+import unicornfinder.unicornfinder.domain.Member;
 import unicornfinder.unicornfinder.repository.CompanyRepository;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,7 +27,65 @@ class CompanyServiceTest {
 
     @Test
     public void 전체조회() throws Exception{
+        /*//컴퍼니 저장 메소드 호출
+        Company company = saveCompany();
+        //테스트용 interest 저장
+        Interest interest = new Interest();
+
+        interest.setCompanyId(company);
+
+        companyRepository.saveInterest(interest);
+
+        Interest interest2 = new Interest();
+
+        interest2.setCompanyId(company);
+
+        companyRepository.saveInterest(interest2);
+*/
         List<Company> companyList= companyService.findCompanies();
+        for(Company c : companyList)
+            System.out.println("count : "+c.getCount()+" "+
+                    "id : "+c.getId()+" "+
+                    "name : "+c.getName()+" ");
+    }
+
+   /* private Company saveCompany(){
+        //테스트용 company 저장
+        Company company = new Company();
+
+        company.setName("우아한형제들");
+        company.setProduct("배달의민족");
+
+        companyRepository.save(company);
+
+        return company;
+    }*/
+
+    @Test
+    public void 검색(){
+/*
+
+        Company company = saveCompany();
+
+        //테스트용 interest 저장
+        Interest interest = new Interest();
+
+        interest.setCompanyId(company);
+
+        companyRepository.saveInterest(interest);
+
+        Interest interest2 = new Interest();
+
+        interest2.setCompanyId(company);
+
+        companyRepository.saveInterest(interest2);
+*/
+
+        List<Company> companyList= companyService.searchCompanies("우아한");
+        if(companyList.isEmpty()){
+            System.out.println("================================================");
+            System.out.println("조회결과 0");
+        }
         for(Company c : companyList)
             System.out.println("count : "+c.getCount()+" "+
                     "id : "+c.getId()+" "+
