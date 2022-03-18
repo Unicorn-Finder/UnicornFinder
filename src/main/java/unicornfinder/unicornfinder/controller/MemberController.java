de4d4f9b1f78f2c78253e8b5bbf863a622032bd8
Goun96 <gowoun96@gmail.com>
2022-03-18 18:45:21 +0900

    FEAT : index 화면 company list index 화면에 company list 띄우기

--- src/main/java/unicornfinder/unicornfinder/controller/MemberController.java
+++ src/main/java/unicornfinder/unicornfinder/controller/MemberController.java
@@ -5,14 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import unicornfinder.unicornfinder.config.auth.dto.SessionMember;
+import unicornfinder.unicornfinder.domain.Company;
+import unicornfinder.unicornfinder.domain.Member;
+import unicornfinder.unicornfinder.service.CompanyService;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
     private final HttpSession httpSession;
+
+    private final CompanyService companyService;
 
     @GetMapping("/")
     public String index(Model model) {
@@ -22,6 +28,10 @@ public class MemberController {
         if (member != null) {
             model.addAttribute("memberName", member.getName());
         }
+
+        //Company 전체 검색
+        List<Company> companies = companyService.findCompanies();
+        model.addAttribute("companies", companies);
         return "index";
     }
 }
