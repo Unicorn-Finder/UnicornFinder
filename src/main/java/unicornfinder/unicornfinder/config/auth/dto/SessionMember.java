4b4d83301be477ce01f1611bcff5eddbd3167791
Goun96 <35549825+Goun96@users.noreply.github.com>
2022-03-19 01:02:52 +0900

    Revert "Feat/company/main"

--- src/main/java/unicornfinder/unicornfinder/config/auth/dto/SessionMember.java
+++ src/main/java/unicornfinder/unicornfinder/config/auth/dto/SessionMember.java
@@ -2,7 +2,6 @@ package unicornfinder.unicornfinder.config.auth.dto;
 
 import lombok.Getter;
 import unicornfinder.unicornfinder.domain.Member;
-import unicornfinder.unicornfinder.domain.Role;
 
 import java.io.Serializable;
 
@@ -11,12 +10,9 @@ import java.io.Serializable;
 public class SessionMember implements Serializable {
     private String name;
     private String email;
-    //session에 role 추가(관리자인지 알아보기 위해)
-    private Role role;
 
     public SessionMember(Member member) {
         this.name = member.getName();
         this.email = member.getEmail();
-        this.role = member.getRole();
     }
 }
