323c55ecf1a8efc4815fd8403a7e72e342b86b23
Goun96 <gowoun96@gmail.com>
2022-03-24 20:45:45 +0900

    FEAT : Comapny update company 수정 기능 추가

--- src/main/java/unicornfinder/unicornfinder/controller/CompanyForm.java
+++ src/main/java/unicornfinder/unicornfinder/controller/CompanyForm.java
@@ -14,7 +14,7 @@ public class CompanyForm {
     private String name; // 회사명
 
     private String product; // 서비스 명
-    private int invest; // 총 투자액
+    private Long invest; // 총 투자액
 
     @Enumerated(EnumType.STRING)
     private Round round; // 투자 단계
@@ -27,6 +27,8 @@ public class CompanyForm {
 
     private int count; //관심수
 
-    private Long id; // 등록한 사람
+    private Long id; //company id
+    
+    private Long register_id; //등록한 사람
 
 }
