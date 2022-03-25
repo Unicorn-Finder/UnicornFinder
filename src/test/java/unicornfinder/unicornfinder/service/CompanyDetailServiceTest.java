a6c6f8af76b76a3e3682b50afb4091c7715970c8
yoo2wo <wogud758787@naver.com>
2022-03-25 15:53:58 +0900

    Chore : 배포 테스트를 위한 유닛테스트 주석 처리

--- src/test/java/unicornfinder/unicornfinder/service/CompanyDetailServiceTest.java
+++ src/test/java/unicornfinder/unicornfinder/service/CompanyDetailServiceTest.java
@@ -1,65 +1,63 @@
-package unicornfinder.unicornfinder.service;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
-import unicornfinder.unicornfinder.controller.CompanyForm;
-import unicornfinder.unicornfinder.domain.*;
-import unicornfinder.unicornfinder.repository.CompanyDetailRepository;
-import unicornfinder.unicornfinder.repository.InterestRepository;
-import unicornfinder.unicornfinder.repository.MemberRepository;
-
-import javax.persistence.EntityManager;
-
-@SpringBootTest
-@Transactional
-@Rollback(value = false)//db 테스트를 위해 롤백 취소
-class CompanyDetailServiceTest {
-
-    @Autowired MemberRepository memberRepository;
-    @Autowired CompanyDetailService companyDetailService;
-    @Autowired CompanyDetailRepository companyDetailRepository;
-    @Autowired InterestRepository interestRepository;
-    @Autowired InterestService interestService;
-    @Autowired CompanyService companyService;
-
-//    /** createCompany 매개변수 변경에 따라 테스트 부분 변경*/
-//    @Test
-//    void 회사등록() throws Exception{
-//        //given
-//        CompanyForm companyForm = new CompanyForm("우아한 형제들", "배달의 민족", 1000000000, Round.SERIES_E, "delivery", 500, Location.SEOUL_SONGPA, 0, null);
-//        Company company = Company.createCompany(companyForm);
-//        //when
-//        Long createId = companyService.create(company);
-//        //then
-//        Assertions.assertThat(company).isEqualTo(companyService.findOne(createId));
+//package unicornfinder.unicornfinder.service;
+//
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//import unicornfinder.unicornfinder.controller.CompanyDetailForm;
+//import unicornfinder.unicornfinder.controller.CompanyForm;
+//import unicornfinder.unicornfinder.domain.*;
+//import unicornfinder.unicornfinder.repository.CompanyDetailRepository;
+//import unicornfinder.unicornfinder.repository.InterestRepository;
+//import unicornfinder.unicornfinder.repository.MemberRepository;
+//
+//import javax.persistence.EntityManager;
+//
+//@SpringBootTest
+//@Transactional
+//class CompanyDetailServiceTest {
+//
+//    @Autowired MemberRepository memberRepository;
+//    @Autowired CompanyDetailService companyDetailService;
+//    @Autowired CompanyDetailRepository companyDetailRepository;
+//    @Autowired InterestRepository interestRepository;
+//    @Autowired InterestService interestService;
+//    @Autowired CompanyService companyService;
+//    @Autowired EntityManager em;
+//
+//
+//    @AfterEach
+//    void cleanup(){
+//        System.out.println("---- cleanup -----");
+//        companyDetailRepository.deleteAll();
 //    }
 //
 //    @Test
-//    void 회사상세조회() throws Exception {
+//    void 회사상세조회()  throws Exception{
 //        //given
-//        CompanyForm companyForm = new CompanyForm("우아한 형제들", "배달의 민족", 1000000000, Round.SERIES_E, "delivery", 500, Location.SEOUL_SONGPA, 0, null);
-//        Company company = Company.createCompany(companyForm);
+//        CompanyForm companyForm = createForm(20, 80000000, "스프링", "스프링파인더");
 //        //when
-//        Long createId = companyService.create(company);
+//        Company company = Company.createCompany(companyForm);
+//        Long id = companyService.create(company);
+//        em.flush();
 //        //then
-//        Assertions.assertThat(company.getCompanyDetail()).isEqualTo(companyService.findOne(createId).getCompanyDetail());
-//
+//        Assertions.assertThat(company.getCompanyDetail()).isEqualTo(companyService.findOne(id).getCompanyDetail());
 //    }
 //
 //    @Test
 //    void 회사상세수정() {
 //        //given
-//        CompanyForm companyForm = new CompanyForm("우아한 형제들", "배달의 민족", 1000000000, Round.SERIES_E, "delivery", 500, Location.SEOUL_SONGPA, 0, null);
+//        CompanyForm companyForm = createForm(30, 40000000, "유니콘", "유니콘파인더");
 //        Company company = Company.createCompany(companyForm);
-//        Long createId = companyService.create(company);
-//        //when //info 추가
-//        companyDetailService.updateCompanyDetail(company.getCompanyDetail().getId(), "이기업은~~","www.bm.com", "맥북 지원", "기술 이사 김영한","서울특별시 송파구 방이2동 위례성대로 2");
+//        Long id = companyService.create(company);
+//
+//        CompanyDetailForm detailForm = createDetailForm("www.bm.com", "최근에 투자받음", "서울시 송파구 xx", "이기업은 ~", "재택근무");
+//        //when
+//        companyDetailService.updateCompanyDetail(id, detailForm);
 //        //then
-//        Assertions.assertThat(companyService.findOne(createId).getCompanyDetail().getBlog()).isEqualTo("www.bm.com");
+//        Assertions.assertThat(companyService.findOne(id).getCompanyDetail().getBlog()).isEqualTo("www.bm.com");
 //    }
 //
 //    @Test
@@ -74,7 +72,7 @@ class CompanyDetailServiceTest {
 //        Member member2 = new Member("bbb", "jh@gmail.com", Role.ADMIN);
 //        memberRepository.save(member2);
 //
-//        CompanyForm companyForm = new CompanyForm("우아한 형제들", "배달의 민족", 1000000000, Round.SERIES_E, "delivery", 500, Location.SEOUL_SONGPA, 0, null);
+//        CompanyForm companyForm = createForm(30, 40000000, "유니콘", "유니콘파인더");
 //        Company company = Company.createCompany(companyForm);
 //        Long createId = companyService.create(company);
 //        //when
@@ -86,13 +84,26 @@ class CompanyDetailServiceTest {
 //
 //        interestService.interest(member2.getId(), createId);
 //        System.out.println(company.getCount());
-//
-//        interestService.cancelInterest(id);
-//        System.out.println(company.getCount());
-//        //then
 //    }
-
-
-
-
-}
+//
+//    private CompanyDetailForm createDetailForm(String blog, String etc, String location, String info, String welfare) {
+//        CompanyDetailForm companyDetailForm = new CompanyDetailForm();
+//        companyDetailForm.setBlog(blog);
+//        companyDetailForm.setEtc(etc);
+//        companyDetailForm.setLocation(location);
+//        companyDetailForm.setInfo(info);
+//        companyDetailForm.setWelfare(welfare);
+//        return companyDetailForm;
+//    }
+//
+//    private CompanyForm createForm(int employee, int invest, String name, String product) {
+//        CompanyForm companyForm = new CompanyForm();
+//        companyForm.setEmployee(employee);
+//        companyForm.setInvest(invest);
+//        companyForm.setName(name);
+//        companyForm.setProduct(product);
+//        return companyForm;
+//    }
+//
+//
+//}
