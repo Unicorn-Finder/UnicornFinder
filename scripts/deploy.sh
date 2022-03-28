764f144d5e756db1bdaffc27f431bd98c13ca8c4
yoo2wo <wogud758787@naver.com>
2022-03-28 10:54:29 +0900

    Chore : 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,5 +33,4 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real.yml \
-    $JAR_NAME /dev/null 2> /dev/null < /dev/null &
+    -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real.yml $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
